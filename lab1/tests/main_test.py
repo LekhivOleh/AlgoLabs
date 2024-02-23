b28ebbd008de398e8193c8a8a32093f8050851c7
lelouchTheGOAT <oleh.lekhiv.ir.2023@lpnu.ua>
2024-02-23 12:08:49 +0200

    update main_test

--- lab1/tests/main_test.py
+++ lab1/tests/main_test.py
@@ -16,12 +16,16 @@ class TestGetLongestPeak(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_no_peak_array(self):
-        result = get_longest_peak([1, 1, 1, 1, 1, 1, 1, 1])
+        result = get_longest_peak([1, 1, 2, 2, 3, 3, 4, 4])
         self.assertEqual(result, 0)
 
     def test_three_peaks_array(self):
         result = get_longest_peak([1, 2, 3, 4, 2, 4, 5, 6, 3, 7, 8, 9, 2, 1])
         self.assertEqual(result, 6)
+
+    def test_given_case(self):
+        result = get_longest_peak([8, 9, 11, 18, 5, 3, -2, 9, 11, 17, 19, 8, 6])
+        self.assertEqual(result, 7)
 
 
 if __name__ == "__main__":
