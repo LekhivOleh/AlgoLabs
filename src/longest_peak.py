7b68bb22fad65afc05d27e4b5c529a4bf3e943f1
lelouchTheGOAT <oleh.lekhiv.ir.2023@lpnu.ua>
2024-02-28 19:11:34 +0200

    deleted unnesessesary prints in longest_peak.py

--- src/longest_peak.py
+++ src/longest_peak.py
@@ -17,11 +17,3 @@ def get_longest_peak(arr):
     return result
 
 
-# print(get_longest_peak([1,3,5,4,2,9,3,7]))
-# print(get_longest_peak([1,2,3,4,5,6,7,8]))
-# print(get_longest_peak([8,7,6,5,4,3,2,1]))##
-# print(get_longest_peak([1,2]))
-# print(get_longest_peak([1,2,3,2,5,6]))
-# print(get_longest_peak([1,3,2,4,5,6,7,4,3,2,8,11,10,9]))
-# print(get_longest_peak([1,3,2,4,5,6,7,4,3,2,1,8,11,10,9]))
-# print(get_longest_peak([1]))
