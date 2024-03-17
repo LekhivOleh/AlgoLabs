2538e2e8a49dd82a63b0736b67a1d4cbf71d0f5b
Lekhiv Oleh <oleh.lekhiv.ir.2023@lpnu.ua>
2024-03-17 04:08:59 +0200

    add new test case

--- tests/get_max_diameter_test.py
+++ tests/get_max_diameter_test.py
@@ -23,6 +23,22 @@ class TestBinaryTreeFunctions(unittest.TestCase):
 
         self.assertEqual(find_max_diameter(root), 6)
 
+    def test_new_tree(self):
+        root = BinaryTree(50)
+        root.right = BinaryTree(80)
+        root.left = BinaryTree(31)
+        root.left.left = BinaryTree(28)
+        root.left.right = BinaryTree(40)
+        root.left.left.left = BinaryTree(20)
+        root.left.left.left.left = BinaryTree(18)
+        root.left.left.left.right = BinaryTree(25)
+        root.left.right.left = BinaryTree(39)
+        root.left.right.right = BinaryTree(42)
+        root.left.right.right.right = BinaryTree(45)
+        root.left.right.right.right.right = BinaryTree(48)
+
+        self.assertEqual(find_max_diameter(root), 7)
+
 
 if __name__ == 'main':
     unittest.main()
