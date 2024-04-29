233cf529477066544551dab2a16baa38670af18e
LekhivOleh <150457849+LekhivOleh@users.noreply.github.com>
2024-04-30 01:07:20 +0300

    Add trie (#7)
    * add trie and test files
    
    * fix bug with searching word
    
    * fix shortest_safe_route test inputs

---
+++ src/trie.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+
+    def search_prefix(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+def create_trie_from_patterns(patterns):
+    if not patterns:
+        return Trie()
+    trie = Trie()
+    for pattern in patterns:
+        trie.insert(pattern)
+    return trie
