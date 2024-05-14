138f62716c60a6e2d818543a284561e4d9f84bfe
Lekhiv Oleh <oleh.lekhiv.ir.2023@lpnu.ua>
2024-05-14 23:37:34 +0300

    change dict name to more understandable

--- src/w_chain.py
+++ src/w_chain.py
@@ -9,13 +9,13 @@ def find_max_sequence_words(words: list[str]) -> int:
     """
     Takes sorted list of words and returns the length of the longest chain of words using dynamic programming.
     """
-    dp = {word: 1 for word in words}
+    words_combinations = {word: 1 for word in words}
     for word in words:
         for i in range(len(word)):
             new_word = word[:i] + word[i+1:]  # Removing one character from the word using slicing
-            if new_word in dp:
-                dp[word] = max(dp[word], dp[new_word] + 1)
-    return max(dp.values())
+            if new_word in words_combinations:
+                words_combinations[word] = max(words_combinations[word], words_combinations[new_word] + 1)
+    return max(words_combinations.values())
 
 
 def write_output(filename: str, result: int) -> None:
